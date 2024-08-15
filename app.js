e843a3db241def9a88bb2c7748c8d9a47b6d73bb
Viwyn <riaruplays@gmail.com>
2024-08-15 14:15:29 +0800

    hero page content and animation

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ function hideSidebar() {
     sidebar.classList.toggle('open')
 
 }
+
+const obfuscatedElement = document.querySelector('.obfuscated');
+
+function getRandomChar() {
+    const chars = 'ⅳ℅℃ℶⅉⅮ⅔℔ↁ⅓⅖⅑⅍ⅽ℆ⅵⅴÅ';
+    return chars[Math.floor(Math.random() * chars.length)];
+}
+
+function obfuscateText(text) {
+    return text.split('').map(char => getRandomChar()).join('');
+}
+
+let delay = 8; //in seconds
+let intervalId;
+
+setTimeout(() => {
+    intervalId = setInterval(() => {
+        const originalText = obfuscatedElement.textContent;
+        obfuscatedElement.textContent = obfuscateText(originalText);
+    }, 50);
+}, delay * 1000);
